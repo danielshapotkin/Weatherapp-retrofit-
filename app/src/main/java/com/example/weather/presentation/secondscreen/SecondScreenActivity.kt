da7fc31d2f8c011008e85fa1fb2ab03bc2e08fb4
Daniel Shapotkin <danielshapotkin@gmail.com>
2024-04-16 10:46:33 +0300

    add ViewModel

--- app/src/main/java/com/example/weather/presentation/secondscreen/SecondScreenActivity.kt
+++ app/src/main/java/com/example/weather/presentation/secondscreen/SecondScreenActivity.kt
@@ -4,22 +4,17 @@ import android.view.View
 import android.widget.TextView
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import com.example.weather.DataModel
 import com.example.weather.R
 import com.example.weather.data.repository.WeatherRepository
 import com.example.weather.retrofit.WeatherNetwork
 
 class SecondScreenActivity : AppCompatActivity() {
-    private val dataModel = DataModel()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_secondscreen)
         val textView = findViewById<TextView>(R.id.textView)
 
-        dataModel.weatherInfo.observe(this) { weatherInfo ->
-            // Обновление UI при изменении сообщения
-            textView.text = weatherInfo
-        }
+
 
 
 
